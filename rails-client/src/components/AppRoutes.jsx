d107922b2a7b34861e7d9b4a5ff3c880329bec27
Manfred Yau <manfred3431@outlook.com>
2024-11-23 18:24:08 +0800

    Implemented edit functionality for posts

--- rails-client/src/components/AppRoutes.jsx
+++ rails-client/src/components/AppRoutes.jsx
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import PostsList from "../features/posts/PostsList";
 import PostDetails from "../features/posts/PostDetails.jsx";
 import NewPostForm from "../features/posts/NewPostForm.jsx";
+import EditPostForm from "../features/posts/EditPostForm.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<PostsList />} />
       <Route path="posts/:id" element={<PostDetails />} />
+      <Route path="posts/:id/edit" element={<EditPostForm />} />
       <Route path="/new" element={<NewPostForm />} />
     </Routes>
   );
