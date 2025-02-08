838727ad5a460469c9178ddb5d9f59df4ae2638c
Manfred Yau <manfred3431@outlook.com>
2025-02-09 00:34:34 +0800

    Add a function that can convert a FormData object to a plain object.

--- rails-client/src/utils/FormDataHelper.js
+++ rails-client/src/utils/FormDataHelper.js
@@ -41,3 +41,11 @@ export function objectToFormData(
   }
   return formData;
 }
+
+export function formDataToObject(formData) {
+  const obj = {};
+  for (let key of formData.keys()) {
+    obj[key] = formData.get(key);
+  }
+  return obj;
+}
